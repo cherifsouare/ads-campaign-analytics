1f3350ecdf80eac28d583edb9a204253b5cf443a
Cherif Souare <chesou88@gmail.com>
2024-02-11 22:23:52 +0000

    Add precommit config file Add precommit config file to enforce best practices in local development

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,16 +2,16 @@
 
 All notable changes to this project will be documented in this file.
 
-The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
-and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to
+[Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## v1.0.0 - 2024-01-27
 
 ### Added
 
-* Add comments for mage files (https://github.com/cherifsouare/ads-campaign-analytics/pull/7)
-* Add steps in VM config GCP (https://github.com/cherifsouare/ads-campaign-analytics/pull/8)
-* Add README.md for terraform documentation (https://github.com/cherifsouare/ads-campaign-analytics/pull/9)
+- Add comments for mage files (https://github.com/cherifsouare/ads-campaign-analytics/pull/7)
+- Add steps in VM config GCP (https://github.com/cherifsouare/ads-campaign-analytics/pull/8)
+- Add README.md for terraform documentation (https://github.com/cherifsouare/ads-campaign-analytics/pull/9)
 
 ## v0.1.0 - 2024-01-20
 
--- mage-files/load_data_block.py
+++ mage-files/load_data_block.py
@@ -1,12 +1,14 @@
-from mage_ai.settings.repo import get_repo_path
+from os import path
+from typing import Dict, List
+
+from google.cloud import storage
 from mage_ai.io.config import ConfigFileLoader
 from mage_ai.io.google_cloud_storage import GoogleCloudStorage
-from google.cloud import storage
-from typing import List,Dict
-from os import path
-if 'data_loader' not in globals():
+from mage_ai.settings.repo import get_repo_path
+
+if "data_loader" not in globals():
     from mage_ai.data_preparation.decorators import data_loader
-if 'test' not in globals():
+if "test" not in globals():
     from mage_ai.data_preparation.decorators import test
 
 
@@ -19,21 +21,19 @@ def load_from_google_cloud_storage(*args, **kwargs):
         Anything (e.g. data frame, dictionary, array, int, str, etc.)
     """
     # Specify your data loading logic here
-    config_path = path.join(get_repo_path(), 'io_config.yaml')
-    config_profile = 'default'
-    
-    bucket_name = 'my-ads-data-source'
+    config_path = path.join(get_repo_path(), "io_config.yaml")
+    config_profile = "default"
+
+    bucket_name = "my-ads-data-source"
     print(args)
- 
-    object_key = args[0]['key']
+
+    object_key = args[0]["key"]
 
     data = GoogleCloudStorage.with_config(ConfigFileLoader(config_path, config_profile)).load(
         bucket_name,
         object_key,
     )
 
-    data = data.astype({
-       
-    })
-    
+    data = data.astype({})
+
     return data
